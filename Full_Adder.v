88a37844a559a6c686b2057f0ec9f225b3a4d4de
CaoMengxi <3054428602@qq.com>
2017-03-28 23:01:10 +0800

    Do some optimizations.

--- Full_Adder.v
+++ Full_Adder.v
@@ -1,12 +1,11 @@
 `timescale 1ns / 1ps
 
 module Full_Adder(in1,in2,cin,sumout,cout);
-	input[0:0] in1,in2,cin;
-	output[0:0] sumout,cout;
+	parameter n;
+	input[n-1:0] in1,in2;
+	input cin;
+	output[n-1:0] sumout;
+	output cout;
 	reg sumout,cout;
-	initial
-		fork
-			assign sumout=(in1^in2)^in2;
-			assign cout=((in1^in2)&in2)|(in1&in2);
-		join
+	assign {cout,sumout}=in1+in2+cin;
 endmodule
